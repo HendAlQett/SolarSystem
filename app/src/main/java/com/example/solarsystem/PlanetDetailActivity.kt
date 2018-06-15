5fe059d05a777a75f08b3bfbb42ba707aa19990f
Hend Mahmoud <hend.alqett@gmail.com>
2018-06-15 04:41:50 +0200

    feat: add ankle for design and use it with Snackbar, Toast, Fragments

--- app/src/main/java/com/example/solarsystem/PlanetDetailActivity.kt
+++ app/src/main/java/com/example/solarsystem/PlanetDetailActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_planet_detail.*
+import org.jetbrains.anko.design.snackbar
+import org.jetbrains.anko.support.v4.withArguments
+import org.jetbrains.anko.toast
 
 class PlanetDetailActivity : AppCompatActivity() {
 
@@ -16,17 +19,15 @@ class PlanetDetailActivity : AppCompatActivity() {
 
         fab.setOnClickListener { view ->
 
-            view.showSnackbar("My message")
+            snackbar(view,"My message", "Action"){
+                toast("inside the action")
+            }
         }
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         if (savedInstanceState == null) {
-            val arguments = Bundle()
-            arguments.putString(ARG_ITEM_ID, intent.getStringExtra(ARG_ITEM_ID))
-
-            val fragment = PlanetDetailFragment()
-            fragment.arguments = arguments
+            val fragment = PlanetDetailFragment().withArguments(ARG_ITEM_ID to intent.getStringExtra(ARG_ITEM_ID))
             supportFragmentManager.beginTransaction()
                     .add(R.id.planet_detail_container, fragment)
                     .commit()
