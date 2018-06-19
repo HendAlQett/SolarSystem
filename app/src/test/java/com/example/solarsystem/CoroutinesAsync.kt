b4926589c1c8b5ad1a36a2a7f130c074a733d968
Hend Mahmoud <hend.alqett@gmail.com>
2018-06-19 02:42:30 +0200

    feat: create and use a new suspend function

--- app/src/test/java/com/example/solarsystem/CoroutinesAsync.kt
+++ app/src/test/java/com/example/solarsystem/CoroutinesAsync.kt
@@ -15,7 +15,7 @@ class CoroutinesAsyncTest {
             delay(1_000)
             println("Inside...")
         }
-        Thread.sleep(1_500)
+        Thread.sleep(1_500L)
 
         println("Stopping...")
     }
@@ -27,7 +27,7 @@ class CoroutinesAsyncTest {
 
         launch(CommonPool) {
             val result: Deferred<Int> = async(CommonPool) {
-                delay(1_000)
+                delay(1_000L)
                 println("Inside...")
                 42
             }
@@ -37,7 +37,7 @@ class CoroutinesAsyncTest {
             println("The result: ${result.await()}")
         }
 
-        Thread.sleep(1_500)
+        Thread.sleep(1_500L)
 
         println("Stopping...")
 
@@ -49,24 +49,49 @@ class CoroutinesAsyncTest {
 
         launch(CommonPool) {
             val result: Deferred<Int> = async(CommonPool) {
-                delay(1_000)
-                println("Inside...")
+                delay(1_000L)
+                println("Inside first...")
                 42
             }
 
             val result2: Deferred<Int> = async(CommonPool) {
-                delay(1_000)
-                println("Inside...")
+                delay(1_000L)
+                println("Inside second...")
                 8
             }
 
             //the .await() function allows the implementing of this method to be suspended until those asynchronous coroutines are completed
-            println("The result: ${result.await() + result2.await()}")
+            println("The sum: ${result.await() + result2.await()}")
         }
 
-        Thread.sleep(1_500)
+        Thread.sleep(1_500L)
 
         println("Stopping...")
     }
+
+    @Test
+    fun usingAsyncWithSuspendFunction() {
+        println("Starting...")
+
+        launch(CommonPool) {
+            val result: Deferred<Int> = async(CommonPool) {
+                ourWork()
+            }
+
+            println("The result: ${result.await()}")
+        }
+
+        Thread.sleep(1_500L)
+
+        println("Stopping...")
+    }
+
+    private suspend fun ourWork(): Int {
+        //We can only call our suspending function from another one or suspending lambda
+        delay(1_000L)
+        println("Inside...")
+        return 42
+    }
+
 
 }
