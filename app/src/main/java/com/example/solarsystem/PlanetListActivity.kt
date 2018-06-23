96e4de2304d2563174f3ad7246d3b9424eec4fd5
Hend Mahmoud <hend.alqett@gmail.com>
2018-06-23 04:52:57 +0200

    feat: adding initial code for menu options

--- app/src/main/java/com/example/solarsystem/PlanetListActivity.kt
+++ app/src/main/java/com/example/solarsystem/PlanetListActivity.kt
@@ -3,9 +3,7 @@ package com.example.solarsystem
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import com.example.solarsystem.dummy.Planet
 import com.example.solarsystem.dummy.PlanetsDataProvider
 import kotlinx.android.extensions.LayoutContainer
@@ -22,6 +20,8 @@ import org.jetbrains.anko.warn
 
 class PlanetListActivity : AppCompatActivity(), AnkoLogger {
 
+    private lateinit var planetsAdapter: PlanetsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_planet_list)
@@ -31,27 +31,23 @@ class PlanetListActivity : AppCompatActivity(), AnkoLogger {
         getPlanetsTimeConsuming()
     }
 
-//    private fun setupRecyclerView(recyclerView: RecyclerView) {
-//
-//        recyclerView.adapter = PlanetsAdapter(PlanetsDataProvider.ITEMS)
-//    }
-
     private fun getPlanetsTimeConsuming() {
         launch(UI) {
-            info("starting launch on ${Thread.currentThread().name}")
+            info("starting launch on ${Thread.currentThread().name}") //This is main thread
 
-            val planets = bg {
+            val planets = bg { //This is a background thread
                 info("starting launch on ${Thread.currentThread().name}")
                 Thread.sleep(3_000L)
                PlanetsDataProvider.ITEMS
             }
 
-            planet_list.adapter = PlanetsAdapter(planets.await())
+            planetsAdapter = PlanetsAdapter(planets.await())
+            planet_list.adapter = planetsAdapter;
         }
     }
 
     inner class PlanetsAdapter internal constructor(
-            private val values: List<Planet>
+            internal var values: List<Planet>
     ) : RecyclerView.Adapter<PlanetsAdapter.ViewHolder>() {
 
         private val clickListener = View.OnClickListener { view ->
@@ -80,4 +76,31 @@ class PlanetListActivity : AppCompatActivity(), AnkoLogger {
         inner class ViewHolder(override val containerView: View) :
                 RecyclerView.ViewHolder(containerView), LayoutContainer
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_planet_list, menu)
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_filter_none -> {
+                planetsAdapter.values = PlanetsDataProvider.ITEMS
+                planetsAdapter.notifyDataSetChanged()
+                return true
+            }
+
+            R.id.action_filter_terrestrial -> {
+
+            }
+
+            R.id.action_filter_gas_giants -> {
+
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
